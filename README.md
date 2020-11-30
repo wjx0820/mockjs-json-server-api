fa1b9d0f90279c61ff4c97f4170c65760074ef67
Jexxie <1989wjx@gmail.com>
2020-11-30 17:19:44 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## 操作步骤
+
+`npm install`
+`node mock.js` 生成 data.json
---
+++ mock.js
@@ -0,0 +1,21 @@
+const Mock = require("mockjs");
+const fs = require("fs");
+
+const data = Mock.mock({
+  // 属性 list 的值是一个数组，其中 10 个元素
+  "emails|10": [
+    {
+      // 属性 id 是一个自增数，起始值为 1，每次增 1
+      "id|+1": 1,
+      // name: "@cname",
+      from: "@email",
+      subject: "@ctitle",
+      body: "@cparagraph(3,4)",
+      sentAt: "@datetime",
+      archived: "@boolean",
+      read: "@boolean",
+    },
+  ],
+});
+
+fs.writeFileSync("db.json", JSON.stringify(data, null, "\t"));
